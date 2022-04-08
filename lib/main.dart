2d7884e8854e4ee4173449519e410e1da6968355
Sreekanth N Kartha <sreekanthnkartha@gmail.com>
2022-04-08 21:52:53 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  
   Widget build(BuildContext context) {
-    
-    return MaterialApp(debugShowCheckedModeBanner: false,home: LoginPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
     // return Hpage();
   }
 }
